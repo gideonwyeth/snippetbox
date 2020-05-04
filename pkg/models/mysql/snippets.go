e4f7fad990532b58412c3804ef4b2acd83bc9efb
gideonwyeth <devnice0@gmail.com>
2020-05-05 00:09:55 +0500

    Add SnippetModel and its methods

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,4 +2,26 @@ package mysql
 
 import (
 	"database/sql"
+
+	"github.com/gideonwyeth/snippetbox/pkg/models"
 )
+
+// define a SnippetModel type which wraps a sql.DB connection pool
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// this will insert a new snippet into the db
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	return 0, nil
+}
+
+// this will return a specific snippet based on its id
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	return nil, nil
+}
+
+// this will return last 10 snippets created
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+	return nil, nil
+}
