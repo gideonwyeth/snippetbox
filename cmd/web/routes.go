15646cecd3d7ee220a816f1c5ff2081d01a013d7
gideonwyeth <devnice0@gmail.com>
2020-05-19 22:17:12 +0500

    Add session middleware to dynamic handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,15 +10,17 @@ import (
 func (app *application) routes() http.Handler {
 	// standard middleware chain for every request
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	// middleware for dynamic application routes
+	dynamicMiddleware := alice.New(app.session.Enable)
 
 	// new ServeMux
 	mux := pat.New()
 
 	// handlers
-	mux.Get("/", http.HandlerFunc(app.home))
-	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
-	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet)) // exact match route before a wildcard
-	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))       // here is the wildcard "id"
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Get("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet)) // exact match route before a wildcard
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))       // here is the wildcard "id"
 
 	// static files server
 	mux.Get("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static/"))))
