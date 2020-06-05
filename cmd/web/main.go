b9d2defa382e5d590898acf6f00a0fc2989b2c13
gideonwyeth <devnice0@gmail.com>
2020-06-06 03:15:52 +0500

    Add user model to app structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,6 +23,7 @@ type application struct {
 	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
+	users         *mysql.UserModel
 }
 
 func main() {
@@ -66,6 +67,7 @@ func main() {
 		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
+		users:         &mysql.UserModel{DB: db},
 	}
 
 	// initialize tls.Config struct to hold non-default TLS settings
